cf7e275c32e4cccf501ddde8c1f38d40165e51d7
ParthibanMoorthi <156990618+ParthibanMoorthi@users.noreply.github.com>
2025-10-04 18:02:59 +0530

    Update UserListPage.jsx

--- src/pages/UserListPage.jsx
+++ src/pages/UserListPage.jsx
@@ -37,9 +37,9 @@ export default function UserListPage() {
 
   const filteredUsers = list.filter(
     (user) =>
-      user.first_name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      user.last_name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      user.email?.toLowerCase().includes(searchTerm.toLowerCase())
+      user.first_name?.toLowerCase()?.includes(searchTerm?.toLowerCase()) ||
+      user.last_name?.toLowerCase()?.includes(searchTerm?.toLowerCase()) ||
+      user.email?.toLowerCase()?.includes(searchTerm?.toLowerCase())
   );
 
   const totalPages = Math.max(1, Math.ceil(filteredUsers.length / usersPerPage));
